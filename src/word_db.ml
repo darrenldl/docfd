8a3ec81d26449ef5b5322e4dacad3a260c612aab
Daren Ldl <darrenldldev@gmail.com>
2023-05-09 13:11:13 +1000

    Optimised word db

--- src/word_db.ml
+++ src/word_db.ml
@@ -1,27 +1,27 @@
 type t = {
   lock : Mutex.t;
   mutable unique_count : int;
-  word_of_index : (int, string) Hashtbl.t;
-  index_of_word : (string, int) Hashtbl.t;
+  word_of_index : (int, string) Kcas_data.Hashtbl.t;
+  index_of_word : (string, int) Kcas_data.Hashtbl.t;
 }
 
 let t : t = {
   lock = Mutex.create ();
   unique_count = 0;
-  word_of_index = Hashtbl.create 40960;
-  index_of_word = Hashtbl.create 40960;
+  word_of_index = Kcas_data.Hashtbl.create ~min_buckets:1000 ();
+  index_of_word = Kcas_data.Hashtbl.create ~min_buckets:1000 ();
 }
 
 let add (word : string) : int =
   Mutex.lock t.lock;
   let index =
-    match Hashtbl.find_opt t.index_of_word word with
+    match Kcas_data.Hashtbl.find_opt t.index_of_word word with
     | Some index -> index
     | None -> (
         let index = t.unique_count in
         t.unique_count <- t.unique_count + 1;
-        Hashtbl.replace t.word_of_index index word;
-        Hashtbl.replace t.index_of_word word index;
+        Kcas_data.Hashtbl.replace t.word_of_index index word;
+        Kcas_data.Hashtbl.replace t.index_of_word word index;
         index
       )
   in
@@ -29,13 +29,7 @@ let add (word : string) : int =
   index
 
 let word_of_index i : string =
-  Mutex.lock t.lock;
-  let x = Hashtbl.find t.word_of_index i in
-  Mutex.unlock t.lock;
-  x
+  Kcas_data.Hashtbl.find t.word_of_index i
 
 let index_of_word s : int =
-  Mutex.lock t.lock;
-  let x = Hashtbl.find t.index_of_word s in
-  Mutex.unlock t.lock;
-  x
+  Kcas_data.Hashtbl.find t.index_of_word s
