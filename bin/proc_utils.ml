e5db3db92b04d3d2101d78099a063a03a92b4a09
Darren Ldl <darrenldldev@gmail.com>
2023-06-11 15:15:23 +1000

    Updated parsing

--- bin/proc_utils.ml
+++ bin/proc_utils.ml
@@ -6,8 +6,12 @@ let run_return_stdout ~proc_mgr (cmd : string list) : string list option =
       let _, stderr = Eio.Process.pipe ~sw proc_mgr in
       let output =
         try
-          let s = Eio.Process.parse_out proc_mgr Eio.Buf_read.take_all ~stderr cmd in
-          Some (String.split_on_char '\n' s)
+          let lines =
+            Eio.Process.parse_out proc_mgr
+              Eio.Buf_read.(map List.of_seq lines)
+              ~stderr cmd
+          in
+          Some lines
         with
         | _ -> None
       in
