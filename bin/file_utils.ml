25ef39a993b995f7481acd51d6487cbd1a0b51ce
Darren Li <darrenldldev@gmail.com>
2023-10-04 14:34:03 +1100

    Updated tmp file naming

--- bin/file_utils.ml
+++ bin/file_utils.ml
@@ -1,5 +1,5 @@
 let read_in_channel_to_tmp_file (ic : in_channel) : (string, string) result =
-  let file = Filename.temp_file "docfd_" "" in
+  let file = Filename.temp_file "docfd-" ".txt" in
   try
     CCIO.with_out file (fun oc ->
         CCIO.copy_into ic oc
