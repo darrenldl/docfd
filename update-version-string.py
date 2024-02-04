99129bc875845dd173f549f0cad86668b35d3b31
Darren Li <darrenldldev@gmail.com>
2024-02-05 00:12:42 +1100

    Updated update-version-string.py to not go through dune-project as there's no more need

--- update-version-string.py
+++ update-version-string.py
@@ -17,18 +17,3 @@ print(f"Writing to {ml_path}")
 with open(ml_path, "w") as f:
     f.write(f"let s = \"{version}\"")
     f.write("\n")
-
-print(f"Replacing version string in dune-project")
-
-dune_project_lines = []
-dune_project_new_lines = []
-
-with open("dune-project") as f:
-    dune_project_lines = f.readlines()
-
-for line in dune_project_lines:
-    dune_project_new_lines.append(re.sub(r"\(version.*", f"(version {version})", line))
-
-with open("dune-project", "w") as f:
-    for line in dune_project_new_lines:
-        f.write(line)
