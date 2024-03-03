82f6982cad80d3ee3e9c2a5f1219917a37c53d77
Darren Li <darrenldldev@gmail.com>
2024-03-03 12:18:34 +1100

    Code refactoring

--- bin/content_and_search_result_render.ml
+++ bin/content_and_search_result_render.ml
@@ -127,37 +127,9 @@ let render_grid
             )
         in
         let content_width = max 1 (width - I.width left_column_label) in
-        let content_lines : Notty.image list list =
-          List.fold_left
-            (fun ((cur_len, acc) : int * Notty.image list list) word ->
-               let word_len = I.width word in
-               let new_len = cur_len + word_len in
-               match acc with
-               | [] -> (new_len, [ [ word ] ])
-               | line :: rest -> (
-                   if new_len > content_width then (
-                     (* If the terminal width is really small,
-                        then this new line may still overflow visually.
-                        But since we still need to put this one word somewhere eventually,
-                        it might as well be here as a line with a single
-                        word.
-
-                        Otherwise we just get an infinite loop where we keep trying
-                        to find a non-existent sufficiently spacious line to put the word.
-                     *)
-                     (word_len, [ word ] :: acc)
-                   ) else (
-                     (new_len, (word :: line) :: rest)
-                   )
-                 )
-            )
-            (0, [])
-            words
-          |> snd
-          |> List.rev_map List.rev
-        in
+        let word_grid = Word_grid.of_images ~width:content_width words in
         let content =
-          content_lines
+          word_grid
           |> List.map I.hcat
           |> I.vcat
         in
