a40fca8316b7c7f15ef08c7a5dff5c14f2c4a19c
Darren Li <darrenldldev@gmail.com>
2024-07-08 22:14:53 +1000

    Minor test code refactoring

--- tests/index_tests.ml
+++ tests/index_tests.ml
@@ -10,12 +10,12 @@ module Qc = struct
 
   let to_of_compressed_gen_from_pages task_pool =
     QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_pages"
-      (index_gen_pages task_pool)
+      (index_gen_from_pages task_pool)
       to_of_compressed_check
 
   let to_of_compressed_gen_from_lines task_pool =
     QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_lines"
-      (index_gen_lines task_pool)
+      (index_gen_from_lines task_pool)
       to_of_compressed_check
 
   let to_of_compressed_string_check index =
@@ -30,12 +30,12 @@ module Qc = struct
 
   let to_of_compressed_string_gen_from_pages task_pool =
     QCheck2.Test.make ~count:100 ~name:"to_of_compressed_string_gen_from_pages"
-      (index_gen_pages task_pool)
+      (index_gen_from_pages task_pool)
       to_of_compressed_string_check
 
   let to_of_compressed_string_gen_from_lines task_pool =
     QCheck2.Test.make ~count:100 ~name:"to_of_compressed_string_gen_from_lines"
-      (index_gen_lines task_pool)
+      (index_gen_from_lines task_pool)
       to_of_compressed_string_check
 
   let suite task_pool = [
--- tests/test_utils.ml
+++ tests/test_utils.ml
@@ -6,7 +6,7 @@ let enriched_token_testable : (module Alcotest.TESTABLE with type t = Search_phr
 let search_phrase_testable : (module Alcotest.TESTABLE with type t = Search_phrase.t) =
   (module Search_phrase)
 
-let index_gen_pages task_pool =
+let index_gen_from_pages task_pool =
   let open QCheck2.Gen in
   map
     (fun pages ->
@@ -15,7 +15,7 @@ let index_gen_pages task_pool =
        |> Index.of_pages task_pool)
     (list_size (int_bound 20) (list_size small_nat (string_size nat)))
 
-let index_gen_lines task_pool =
+let index_gen_from_lines task_pool =
   let open QCheck2.Gen in
   map
     (fun lines ->
