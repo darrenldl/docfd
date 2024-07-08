6f5c79dd28d559f918d7a8adb50bb65675632211
Darren Li <darrenldldev@gmail.com>
2024-07-08 22:11:47 +1000

    Tuning qcheck tests

--- tests/index_tests.ml
+++ tests/index_tests.ml
@@ -2,32 +2,46 @@ open Docfd_lib
 open Test_utils
 
 module Qc = struct
-  let to_of_compressed task_pool =
-    QCheck2.Test.make ~count:100 ~name:"to_of_compressed"
-      (index_gen task_pool)
-      (fun index ->
-         let index' = Index.to_compressed index
-                      |> Index.of_compressed
-         in
-         Index.equal index index'
+  let to_of_compressed_check index =
+    Index.equal
+      index
+      (Index.to_compressed index
+       |> Index.of_compressed)
+
+  let to_of_compressed_gen_from_pages task_pool =
+    QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_pages"
+      (index_gen_pages task_pool)
+      to_of_compressed_check
+
+  let to_of_compressed_gen_from_lines task_pool =
+    QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_lines"
+      (index_gen_lines task_pool)
+      to_of_compressed_check
+
+  let to_of_compressed_string_check index =
+    match
+      Index.to_compressed_string index
+      |> Index.of_compressed_string
+    with
+    | None -> false
+    | Some index' -> (
+        Index.equal index index'
       )
 
-  let to_of_compressed_string task_pool =
-    QCheck2.Test.make ~count:100 ~name:"to_of_compressed_string"
-      (index_gen task_pool)
-      (fun index ->
-         match
-           Index.to_compressed_string index
-           |> Index.of_compressed_string
-         with
-         | None -> false
-         | Some index' -> (
-             Index.equal index index'
-           )
-      )
+  let to_of_compressed_string_gen_from_pages task_pool =
+    QCheck2.Test.make ~count:100 ~name:"to_of_compressed_string_gen_from_pages"
+      (index_gen_pages task_pool)
+      to_of_compressed_string_check
+
+  let to_of_compressed_string_gen_from_lines task_pool =
+    QCheck2.Test.make ~count:100 ~name:"to_of_compressed_string_gen_from_lines"
+      (index_gen_lines task_pool)
+      to_of_compressed_string_check
 
   let suite task_pool = [
-    to_of_compressed task_pool;
-    to_of_compressed_string task_pool;
+    to_of_compressed_gen_from_pages task_pool;
+    to_of_compressed_gen_from_lines task_pool;
+    to_of_compressed_string_gen_from_pages task_pool;
+    to_of_compressed_string_gen_from_lines task_pool;
   ]
 end
