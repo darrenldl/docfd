6e7566957d47fd093d5dada1e2f3335270503fb0
Darren Li <darrenldldev@gmail.com>
2024-07-12 21:38:39 +1000

    Fixed text in qc test name

--- tests/index_tests.ml
+++ tests/index_tests.ml
@@ -52,12 +52,12 @@ module Qc = struct
        |> Option.get)
 
   let to_of_json_gen_from_pages task_pool =
-    QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_pages"
+    QCheck2.Test.make ~count:1000 ~name:"to_of_json_gen_from_pages"
       (index_gen_from_pages task_pool)
       to_of_json_check
 
   let to_of_json_gen_from_lines task_pool =
-    QCheck2.Test.make ~count:1000 ~name:"to_of_compressed_gen_from_lines"
+    QCheck2.Test.make ~count:1000 ~name:"to_of_json_gen_from_lines"
       (index_gen_from_lines task_pool)
       to_of_json_check
 
