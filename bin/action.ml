86278b07a706592e306f67e22b00ce0877dbba5a
Darren Li <darrenldldev@gmail.com>
2024-11-05 10:50:59 +1100

    Switched to using quoted strings for action arguments when applicable

--- bin/action.ml
+++ bin/action.ml
@@ -6,54 +6,101 @@ type t = [
   | `Filter of string
 ]
 
+let pp_escaped fmt (s : string) =
+  let has_single_quote = String.contains s '\'' in
+  let has_double_quote = String.contains s '"' in
+  if has_single_quote && has_double_quote then (
+    Fmt.pf fmt "\"";
+    String.iter (fun c ->
+      if c = '"' then (
+         Fmt.pf fmt "\\"
+      );
+      Fmt.pf fmt "%c" c
+    ) s;
+    Fmt.pf fmt "\"";
+  ) else if has_double_quote then (
+    Fmt.pf fmt "'%s'" s;
+  ) else (
+    Fmt.pf fmt "\"%s\"" s;
+  )
+
 let pp fmt (t : t) =
   match t with
-  | `Drop_path s -> Fmt.pf fmt "drop path %s" s
+  | `Drop_path s -> Fmt.pf fmt "drop path %a" pp_escaped s
   | `Drop_listed -> Fmt.pf fmt "drop listed"
   | `Drop_unlisted -> Fmt.pf fmt "drop unlisted"
   | `Search s -> (
       if String.length s = 0 then (
         Fmt.pf fmt "clear search"
       ) else (
-        Fmt.pf fmt "search %s" s
+        Fmt.pf fmt "search %a" pp_escaped s
       )
     )
   | `Filter s -> (
       if String.length s = 0 then (
         Fmt.pf fmt "clear filter"
       ) else (
-        Fmt.pf fmt "filter %s" s
+        Fmt.pf fmt "filter %a" pp_escaped s
       )
     )
 
 let to_string (t : t) =
   Fmt.str "%a" pp t
 
-let of_string (s : string) : t option =
-  let skip_spaces l =
-    let rec aux l =
-      match l with
-      | "" :: l -> aux l
-      | _ -> l
-    in
-    aux l
-  in
-  let f l = skip_spaces l |> String.concat " " in
-  let l = String.split_on_char ' ' s in
-  match skip_spaces l with
-  | "drop" :: l -> (
-      match skip_spaces l with
-      | "path" :: [] -> None
-      | "path" :: l -> Some (`Drop_path (f l))
-      | "listed" :: [] -> Some `Drop_listed
-      | "unlisted" :: [] -> Some `Drop_unlisted
-      | _ -> None
+module Parsers = struct
+  type t' = t
+
+  open Angstrom
+  open Parser_components
+
+  let quoted_string =
+    let aux quote_char =
+    (char quote_char *>
+    (
+    many1 (
+      take_while1 (fun c ->
+        c <> '\\' && c <> quote_char
+      )
+      <|>
+      (char '\\' *> any_char >>| fun c -> Printf.sprintf "%c" c)
     )
-  | "clear" :: "search" :: [] -> Some (`Search "")
-  | "search" :: l -> Some (`Search (f l))
-  | "clear" :: "filter" :: [] -> Some (`Filter "")
-  | "filter" :: l -> Some (`Filter (f l))
-  | _ -> None
+    )
+    <* char quote_char <* skip_spaces)
+    >>| fun l -> String.concat "" l
+    in
+    choice [
+      aux '\'';
+     aux '"';
+    ]
+
+  let p : t' Angstrom.t =
+    choice [
+      string "drop" *> skip_spaces *> (
+        choice [
+          string "path" *> skip_spaces *> quoted_string >>| (fun s -> (`Drop_path s));
+          string "listed" *> skip_spaces *> return `Drop_listed;
+          string "unlisted" *> skip_spaces *> return `Drop_unlisted;
+        ]
+      );
+      string "clear" *> skip_spaces *> (
+        choice [
+          string "search" *> skip_spaces *> return (`Search "");
+          string "filter" *> skip_spaces *> return (`Filter "");
+        ]
+      );
+      string "search" *> skip_spaces *> (
+        quoted_string >>| (fun s -> (`Search s))
+      );
+      string "filter" *> skip_spaces *> (
+        quoted_string >>| (fun s -> (`Filter s))
+      );
+    ]
+end
+
+let of_string (s : string) : t option =
+  match Angstrom.(parse_string ~consume:Consume.All) Parsers.p s with
+  | Ok t -> Some t
+  | Error _ -> None
 
 let equal (x : t) (y : t) =
   match x, y with
