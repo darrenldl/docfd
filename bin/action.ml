e122a39dd8cc8355818e5244680811869b07b4c0
Darren Li <darrenldldev@gmail.com>
2024-11-05 12:37:31 +1100

    Adjusting action parsing

--- bin/action.ml
+++ bin/action.ml
@@ -35,13 +35,16 @@ module Parsers = struct
   open Angstrom
   open Parser_components
 
+  let any_string_trimmed =
+    any_string >>| String.trim
+
   let p : t' Angstrom.t =
     choice [
       string "drop" *> skip_spaces *> (
         choice [
           string "path" *> skip_spaces *>
           char ':' *> skip_spaces *>
-          any_string >>| (fun s -> (`Drop_path s));
+          any_string_trimmed >>| (fun s -> (`Drop_path s));
           string "listed" *> skip_spaces *> return `Drop_listed;
           string "unlisted" *> skip_spaces *> return `Drop_unlisted;
         ]
@@ -54,10 +57,10 @@ module Parsers = struct
       );
       string "search" *> skip_spaces *>
       char ':' *> skip_spaces *>
-      any_string >>| (fun s -> (`Search s));
+      any_string_trimmed >>| (fun s -> (`Search s));
       string "filter" *> skip_spaces *>
       char ':' *> skip_spaces *>
-      any_string >>| (fun s -> (`Filter s));
+      any_string_trimmed >>| (fun s -> (`Filter s));
     ]
 end
 
