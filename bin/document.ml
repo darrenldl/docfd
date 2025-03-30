925f3d12cee76b6b2f4114baf33615256b661f11
Darren Li <darrenldldev@gmail.com>
2025-03-30 13:53:21 +1100

    Removed old pruning function clean_up_cache_dir

--- bin/document.ml
+++ bin/document.ml
@@ -36,36 +36,6 @@ let refresh_modification_time ~path =
   let time = Unix.time () in
   Unix.utimes path time time
 
-let clean_up_cache_dir ~cache_dir =
-  let all_files =
-    Sys.readdir cache_dir
-    |> Array.to_seq
-    |> Seq.map (fun x ->
-        Filename.concat cache_dir x)
-    |> Seq.filter (fun x ->
-        match File_utils.typ_of_path x with
-        | Some (`File, _) -> File_utils.extension_of_file x = Params.index_file_ext
-        | _ -> false
-      )
-    |> Array.of_seq
-  in
-  let file_count = Array.length all_files in
-  if file_count > !Params.cache_soft_limit + 100 then (
-    let all_files =
-      all_files
-      |> Array.map (fun x ->
-          let stat = Unix.stat x in
-          let modification_time = stat.st_mtime in
-          (x, modification_time)
-        )
-    in
-    Array.sort (fun (_x1, x2) (_y1, y2) -> Float.compare y2 x2) all_files;
-    for i = !Params.cache_soft_limit to file_count - 1 do
-      let path, _mtime = all_files.(i) in
-      Sys.remove path
-    done
-  )
-
 let inter_search_scope (x : Diet.Int.t) (t : t) : t =
   let search_scope =
     match t.search_scope with
