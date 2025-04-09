1e6b3910752ee946368e9c0c4c29356c42bf1548
Darren <darrenldldev@gmail.com>
2025-04-10 02:36:22 +1000

    Reworked Path_open code into using Config.t and cmd pair

--- bin/path_open.ml
+++ bin/path_open.ml
@@ -120,13 +120,12 @@ let parse_spec (s : string) : (string * [ `Foreground | `Background ] * string, 
       | Ok _ -> Ok (ext, fb, cmd)
     )
 
-let xdg_open_cmd ~path =
-  Fmt.str "xdg-open %s" path
+let xdg_open_cmd =
+  "xdg-open {path}"
 
-let pandoc_supported_format ~path =
-  let path = Filename.quote path in
-  let cmd = xdg_open_cmd ~path in
-  Proc_utils.run_in_background cmd |> ignore
+let pandoc_supported_format_config_and_cmd ~path =
+  (Config.make ~path ~fb:`Background (),
+   xdg_open_cmd)
 
 let compute_most_unique_word_and_residing_page_num ~doc_hash found_phrase =
   let page_nums = found_phrase
@@ -188,137 +187,173 @@ let compute_most_unique_word_and_residing_page_num ~doc_hash found_phrase =
   |> (fun (word, page_num, _freq) ->
       (word.found_word, page_num))
 
-let pdf ~doc_hash ~path ~search_result =
-  let path = Filename.quote path in
-  let fallback =
+let pdf_config_and_cmd ~doc_hash ~path ~search_result : Config.t * string =
+  let fallback : Config.t * string =
+    let config = Config.make ~path ~fb:`Background () in
     match Params.os_typ with
-    | `Linux -> xdg_open_cmd ~path
-    | `Darwin -> Fmt.str "open %s" path
+    | `Linux -> (config, xdg_open_cmd)
+    | `Darwin -> (config, "open {path}")
   in
-  let cmd =
-    match search_result with
-    | None -> fallback
-    | Some search_result -> (
-        let found_phrase = Search_result.found_phrase search_result in
-        match Params.os_typ with
-        | `Linux -> (
-            match Xdg_utils.default_desktop_file_path `PDF with
-            | None -> fallback
-            | Some viewer_desktop_file_path -> (
-                let (most_unique_word, most_unique_word_page_num) =
-                  compute_most_unique_word_and_residing_page_num ~doc_hash found_phrase
-                in
-                let flatpak_package_name =
-                  let s = Filename.basename viewer_desktop_file_path in
-                  Option.value ~default:s
-                    (CCString.chop_suffix ~suf:".desktop" s)
-                in
-                let viewer_desktop_file_path_lowercase_ascii =
-                  String.lowercase_ascii viewer_desktop_file_path
-                in
-                let contains sub =
-                  CCString.find ~sub viewer_desktop_file_path_lowercase_ascii >= 0
-                in
-                let page_num = most_unique_word_page_num + 1 in
-                let make_command name args =
-                  resolve_cmd
-                    ~quote_path:false
-                    ~path
-                    ~page_num
-                    ~line_num:0
-                    ~search_word:most_unique_word
-                    (if contains "flatpak" then
-                       Fmt.str "flatpak run %s %s" flatpak_package_name args
-                     else
-                       Fmt.str "%s %s" name args)
-                  |> Option.get
-                in
-                if contains "okular" then
-                  make_command "okular"
-                    "--page {page_num} --find {search_word} {path}"
-                else if contains "evince" then
-                  make_command "evince"
-                    "--page-index {page_num} --find {search_word} {path}"
-                else if contains "xreader" then
-                  make_command "xreader"
-                    "--page-index {page_num} --find {search_word} {path}"
-                else if contains "atril" then
-                  make_command "atril"
-                    "--page-index {page_num} --find {search_word} {path}"
-                else if contains "mupdf" then
-                  make_command "mupdf" "{path} {page_num}"
+  match search_result with
+  | None -> fallback
+  | Some search_result -> (
+      let found_phrase = Search_result.found_phrase search_result in
+      match Params.os_typ with
+      | `Linux -> (
+          match Xdg_utils.default_desktop_file_path `PDF with
+          | None -> fallback
+          | Some viewer_desktop_file_path -> (
+              let (most_unique_word, most_unique_word_page_num) =
+                compute_most_unique_word_and_residing_page_num ~doc_hash found_phrase
+              in
+              let flatpak_package_name =
+                let s = Filename.basename viewer_desktop_file_path in
+                Option.value ~default:s
+                  (CCString.chop_suffix ~suf:".desktop" s)
+              in
+              let viewer_desktop_file_path_lowercase_ascii =
+                String.lowercase_ascii viewer_desktop_file_path
+              in
+              let contains sub =
+                CCString.find ~sub viewer_desktop_file_path_lowercase_ascii >= 0
+              in
+              let page_num = most_unique_word_page_num + 1 in
+              let config =
+                Config.make ~path ~page_num ~search_word:most_unique_word ~fb:`Background ()
+              in
+              let make_command name args =
+                if contains "flatpak" then
+                  Fmt.str "flatpak run %s %s" flatpak_package_name args
                 else
-                  fallback
-              )
-          )
-        | `Darwin -> fallback
-      )
-  in
-  do_if_debug (fun oc ->
-      Printf.fprintf oc "System command: %s\n" cmd
-    );
-  Proc_utils.run_in_background cmd |> ignore
-
-let gen_command_to_open_text_file_to_line_num ~editor ~quote_path ~path ~line_num =
-  let path =
-    if quote_path then
-      Filename.quote path
-    else
-      path
-  in
-  let fallback = Fmt.str "%s {path}" editor in
-  resolve_cmd
-    ~quote_path:false
-    ~path
-    ~page_num:0
-    ~line_num
-    ~search_word:""
-    (match Filename.basename editor with
-     | "nano" ->
-       Fmt.str "%s +{line_num} {path}" editor
-     | "nvim" | "vim" | "vi" ->
-       Fmt.str "%s +{line_num} {path}" editor
-     | "kak" ->
-       Fmt.str "%s +{line_num} {path}" editor
-     | "hx" ->
-       Fmt.str "%s {path}:{line_num}" editor
-     | "emacs" ->
-       Fmt.str "%s +{line_num} {path}" editor
-     | "micro" ->
-       Fmt.str "%s {path}:{line_num}" editor
-     | "jed" | "xjed" ->
-       Fmt.str "%s {path} -g {line_num}" editor
-     | _ ->
-       fallback
+                  Fmt.str "%s %s" name args
+              in
+              if contains "okular" then
+                (config,
+                 make_command "okular"
+                   "--page {page_num} --find {search_word} {path}")
+              else if contains "evince" then
+                (config,
+                 make_command "evince"
+                   "--page-index {page_num} --find {search_word} {path}")
+              else if contains "xreader" then
+                (config,
+                 make_command "xreader"
+                   "--page-index {page_num} --find {search_word} {path}")
+              else if contains "atril" then
+                (config,
+                 make_command "atril"
+                   "--page-index {page_num} --find {search_word} {path}")
+              else if contains "mupdf" then
+                (config, make_command "mupdf" "{path} {page_num}")
+              else
+                fallback
+            )
+        )
+      | `Darwin -> fallback
     )
-  |> Option.get
 
-let text ~doc_hash document_src ~editor ~path ~search_result =
-  let path = Filename.quote path in
-  let fallback = Fmt.str "%s %s" editor path in
-  let cmd =
+let config_and_cmd_to_open_text_file ~path ?line_num () : Config.t * string =
+  let editor = !Params.text_editor in
+  let fallback =
+    (Config.make ~path ~fb:`Foreground (), Fmt.str "%s {path}" editor)
+  in
+  match line_num with
+  | None -> fallback
+  | Some line_num -> (
+      let config =
+        Config.make ~path ~line_num ~fb:`Foreground ()
+      in
+      match Filename.basename editor with
+      | "nano" ->
+        (config,
+         Fmt.str "%s +{line_num} {path}" editor)
+      | "nvim" | "vim" | "vi" ->
+        (config,
+         Fmt.str "%s +{line_num} {path}" editor)
+      | "kak" ->
+        (config,
+         Fmt.str "%s +{line_num} {path}" editor)
+      | "hx" ->
+        (config,
+         Fmt.str "%s {path}:{line_num}" editor)
+      | "emacs" ->
+        (config,
+         Fmt.str "%s +{line_num} {path}" editor)
+      | "micro" ->
+        (config,
+         Fmt.str "%s {path}:{line_num}" editor)
+      | "jed" | "xjed" ->
+        (config,
+         Fmt.str "%s {path} -g {line_num}" editor)
+      | _ ->
+        fallback
+    )
+
+let text_config_and_cmd ~doc_hash ~document_src_is_stdin ~path ~search_result : Config.t * string =
+  let line_num =
     match search_result with
-    | None -> fallback
+    | None -> None
     | Some search_result -> (
         let first_word = List.hd @@ Search_result.found_phrase search_result in
         let first_word_loc =
           Index.loc_of_pos ~doc_hash first_word.Search_result.found_word_pos
         in
-        let line_num = first_word_loc
-                       |> Index.Loc.line_loc
-                       |> Index.Line_loc.line_num_in_page
-                       |> (fun x -> x + 1)
-        in
-        gen_command_to_open_text_file_to_line_num
-          ~editor ~quote_path:false ~path ~line_num
+        first_word_loc
+        |> Index.Loc.line_loc
+        |> Index.Line_loc.line_num_in_page
+        |> (fun x -> x + 1)
+        |> Option.some
       )
   in
-  let cmd =
-    match document_src with
-    | Document_src.Stdin _ -> Fmt.str "</dev/tty %s" cmd
-    | _ -> cmd
+  config_and_cmd_to_open_text_file
+    ~path
+    ?line_num
+    ()
+
+let main ~close_term ~doc_hash ~document_src_is_stdin ~path ~search_result =
+  let ext = File_utils.extension_of_file path in
+  let config, cmd =
+    (match File_utils.format_of_file path with
+     | `PDF -> (
+         pdf_config_and_cmd ~doc_hash ~path ~search_result
+       )
+     | `Pandoc_supported_format -> (
+         pandoc_supported_format_config_and_cmd ~path
+       )
+     | `Text -> (
+         text_config_and_cmd ~doc_hash ~document_src_is_stdin ~path ~search_result
+       )
+    )
+    |> (fun (config, cmd) ->
+        match Hashtbl.find_opt Params.path_open_specs ext with
+        | None -> (
+            (config, cmd)
+          )
+        | Some (fb, cmd) -> (
+            ({ config with fb }, cmd)
+          )
+      )
+    |> (fun (config, cmd) ->
+        (config, Result.get_ok (resolve_cmd config cmd)))
   in
-  do_if_debug (fun oc ->
-      Printf.fprintf oc "System command: %s\n" cmd
-    );
-  Sys.command cmd |> ignore
+  match config.fb with
+  | `Foreground -> (
+      let cmd =
+        if document_src_is_stdin then (
+          Fmt.str "</dev/tty %s" cmd
+        ) else (
+          cmd
+        )
+      in
+      close_term ();
+      do_if_debug (fun oc ->
+          Printf.fprintf oc "System command: %s\n" cmd
+        );
+      Sys.command cmd |> ignore
+    )
+  | `Background -> (
+      do_if_debug (fun oc ->
+          Printf.fprintf oc "System command: %s\n" cmd
+        );
+      Proc_utils.run_in_background cmd |> ignore
+    )
