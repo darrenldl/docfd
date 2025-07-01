c04fa875a839ba217e89e32e94e47d80a84ed0b2
Darren <darrenldldev@gmail.com>
2025-07-02 00:58:22 +1000

    Disabled Lwd unsafe mutation logging

--- bin/docfd.ml
+++ bin/docfd.ml
@@ -561,6 +561,7 @@ let run
        Params.text_editor := editor;
      )
   );
+  Lwd.unsafe_mutation_logger := (fun () -> ());
   let recognized_exts =
     compute_total_recognized_exts ~exts ~additional_exts
   in
