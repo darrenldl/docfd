31264d536ee5b369709951714365c395036a5261
Darren <darrenldldev@gmail.com>
2025-08-08 18:11:27 +1000

    Added committed flag to Document_store_snapshot.t

--- bin/document_store_snapshot.ml
+++ bin/document_store_snapshot.ml
@@ -3,8 +3,11 @@ let counter = ref 0
 type t = {
   last_command : Command.t option;
   store : Document_store.t;
+  committed : bool;
   id : int;
 }
+
+let committed t = t.committed
 
 let last_command t = t.last_command
 
@@ -15,13 +18,13 @@ let id t = t.id
 let equal_id x y =
   id x = id y
 
-let make ~last_command store : t =
+let make ?(committed = true) ~last_command store : t =
   let id = !counter in
   counter := id + 1;
-  { last_command; store; id }
+  { last_command; store; id; committed }
 
-let make_empty () =
-  make ~last_command:None Document_store.empty
+let make_empty ?committed () =
+  make ?committed ~last_command:None Document_store.empty
 
 let update_store store t =
   { t with store }
--- bin/document_store_snapshot.mli
+++ bin/document_store_snapshot.mli
@@ -1,5 +1,7 @@
 type t
 
+val committed : t -> bool
+
 val last_command : t -> Command.t option
 
 val store : t -> Document_store.t
@@ -8,8 +10,8 @@ val id : t -> int
 
 val equal_id : t -> t -> bool
 
-val make : last_command:Command.t option -> Document_store.t -> t
+val make : ?committed:bool -> last_command:Command.t option -> Document_store.t -> t
 
-val make_empty : unit -> t
+val make_empty : ?committed:bool -> unit -> t
 
 val update_store : Document_store.t -> t -> t
