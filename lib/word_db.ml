be4fb670fc1cd15d71cb1c94b0303de802ba059f
Darren <darrenldldev@gmail.com>
2025-08-29 16:21:40 +1000

    Added missing transactions to word db write, moved with_db call into the API itself

--- lib/word_db.ml
+++ lib/word_db.ml
@@ -37,46 +37,52 @@ let index_of_word s : int =
       Hashtbl.find t.index_of_word s
     )
 
-let read_from_db ~db : unit =
+let read_from_db () : unit =
   let open Sqlite3_utils in
   lock (fun () ->
-      Dynarray.clear t.word_of_index;
-      Hashtbl.clear t.index_of_word;
-      iter_stmt ~db
-        {|
+      with_db (fun db ->
+          Dynarray.clear t.word_of_index;
+          Hashtbl.clear t.index_of_word;
+          iter_stmt ~db
+            {|
   SELECT id, word
   FROM word
   |}
-        ~names:[]
-        (fun data ->
-           let id = Data.to_int_exn data.(0) in
-           let word = Data.to_string_exn data.(1) in
-           Dynarray.add_last t.word_of_index word;
-           Hashtbl.replace t.index_of_word word id;
+            ~names:[]
+            (fun data ->
+               let id = Data.to_int_exn data.(0) in
+               let word = Data.to_string_exn data.(1) in
+               Dynarray.add_last t.word_of_index word;
+               Hashtbl.replace t.index_of_word word id;
+            )
         )
     )
 
-let write_to_db ~db : unit =
+let write_to_db () : unit =
   let open Sqlite3_utils in
   lock (fun () ->
-      with_stmt ~db
-        {|
+      with_db (fun db ->
+          step_stmt ~db "BEGIN IMMEDIATE" ignore;
+          with_stmt ~db
+            {|
   INSERT INTO word
   (id, word)
   VALUES
   (@id, @word)
   ON CONFLICT(id) DO NOTHING
   |}
-        (fun stmt ->
-           Dynarray.iteri (fun id word ->
-               bind_names
-                 stmt
-                 [ ("@id", INT (Int64.of_int id))
-                 ; ("@word", TEXT word)
-                 ];
-               step stmt;
-               reset stmt;
-             )
-             t.word_of_index
+            (fun stmt ->
+               Dynarray.iteri (fun id word ->
+                   bind_names
+                     stmt
+                     [ ("@id", INT (Int64.of_int id))
+                     ; ("@word", TEXT word)
+                     ];
+                   step stmt;
+                   reset stmt;
+                 )
+                 t.word_of_index
+            );
+          step_stmt ~db "COMMIT" ignore;
         )
     )
--- lib/word_db.mli
+++ lib/word_db.mli
@@ -6,6 +6,6 @@ val word_of_index : int -> string
 
 val index_of_word : string -> int
 
-val read_from_db : db:Sqlite3.db -> unit
+val read_from_db : unit -> unit
 
-val write_to_db : db:Sqlite3.db -> unit
+val write_to_db : unit -> unit
