a7a61358407cd122e29dbd65ed41de5d60344dcc
Darren <darrenldldev@gmail.com>
2025-09-05 18:43:01 +1000

    Revert "Reworked Document_pipeline to collect all ir0 first to ensure all doc_ids have been allocated"
    This reverts commit 29fa1995d523aa135deea33798b0b6494ae9fae9.

--- bin/document_pipeline.mli
+++ bin/document_pipeline.mli
@@ -1,7 +1,9 @@
 type t
 
-val make : env:Eio_unix.Stdenv.base -> Docfd_lib.Task_pool.t -> Document.Ir0.t Seq.t -> t
+val make : env:Eio_unix.Stdenv.base -> Docfd_lib.Task_pool.t -> t
 
-val run : document_sizes:int String_map.t -> report_progress:(int -> unit) -> t -> unit
+val feed : t -> Search_mode.t -> doc_hash:string -> string -> unit
+
+val run : t -> unit
 
 val finalize : t -> Document.t list
