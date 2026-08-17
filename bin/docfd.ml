97b6ea7ba92e299e8006d4eb4336233ebb894009
Darren <darrenldldev@gmail.com>
2026-08-17 12:52:57 +1000

    Added backtrace for session worker

--- bin/docfd.ml
+++ bin/docfd.ml
@@ -1278,7 +1278,31 @@ let run
   Eio.Fiber.any [
     (fun () ->
        Eio.Domain_manager.run (Eio.Stdenv.domain_mgr env)
-         (fun () -> Session_manager.worker_fiber pool));
+         (fun () ->
+            try
+              Session_manager.worker_fiber pool
+            with
+            | exn -> (
+              let backtrace = Printexc.get_raw_backtrace () in
+              let log oc =
+                Printf.fprintf oc
+                  "Session worker crashed: %s\n%s\n%"
+                  (Printexc.to_string exn)
+                  (Printexc.raw_backtrace_to_string backtrace)
+              in
+              (try
+                 match !Params.debug_output with
+                 | None -> log stderr
+                 | Some oc -> log oc
+               with
+               | _ -> (
+                 try
+                   log stderr
+              with
+              | _ -> ()
+               ));
+              Printexc.raise_with_backtrace exn backtrace
+            )));
     Session_manager.manager_fiber;
     UI_base.Key_binding_info.grid_light_fiber;
     (fun () ->
@@ -1362,6 +1386,7 @@ let cmd ~eio_env ~sw =
      $ paths_arg)
 
 let () =
+  Printexc.record_backtrace true;
   if Sys.win32 then (
     exit_with_error_msg "Windows is not supported"
   );
